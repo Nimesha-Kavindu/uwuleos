ab91d091d925de4a0aebd03132f23a2b9d19cda9
SisuruDisnaka <sisurudisnaka001@gmail.com>
2026-09-06 12:53:37 +0530

    chore: remove .next from repository

--- .next/dev/server/chunks/ssr/_next-internal_server_app_board_page_actions_0lcj6vy.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/board/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_board_page_actions_0lcj6vy.js.map
--- .next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_app_globals_162hn9o.css","static/chunks/src_10ttcmq._.js","static/chunks/node_modules_1elnohw._.js","static/chunks/src_components_0cglxl3._.js","static/chunks/node_modules_lucide-react_dist_esm_icons_01ksdpt._.js"],
-    source: "entry"
-});
